6af8caf0ded66f793a50c87a1d9cfd2628b41127
Abhilash Kumar <abhilashkr31@gmail.com>
2018-12-02 23:34:12 +0530

    mean-stack: Adding Header toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,27 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+   MatInputModule,
+   MatCardModule,
+   MatButtonModule,
+   MatToolbarModule } from '@angular/material/';
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
