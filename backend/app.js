9da14555d71b3fd40ed0d7ae616b9a9dab74a323
Abhilash Kumar <abhilashkr31@gmail.com>
2018-12-15 01:39:50 +0530

    mean-stack: Fetch data from DB

--- backend/app.js
+++ backend/app.js
@@ -42,22 +42,15 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-    const posts = [
-        {
-            id: '1234',
-            title: 'First ServerSide post',
-            content: 'This is coming from the server'
-        },
-        {
-            id: 'djkskjjd',
-            title: 'Second ServerSide post',
-            content: 'This is coming from the server'
-        }
-    ]
-    res.status(200).json({
-        message: "Posts fetched successfully",
-        posts: posts
-    });
+    Post.find()
+        .then(posts => {
+            console.log(posts);
+            res.status(200).json({
+                message: "Posts fetched successfully",
+                posts: posts
+            });
+        });
+    
 });
 
 module.exports = app;
