0f40120f1768a8f05cb83e34c7538f91756f30ff
Abhilash Kumar <abhilashkr31@gmail.com>
2018-12-23 17:09:52 +0530

    mean-stack: Add support to upload images in angular

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -39,12 +39,17 @@ export class PostsService {
         );
     }
 
-    addPost(title: string, content: string) {
-        const post: Post = { id: null, title: title, content: content};
-        this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+    addPost(title: string, content: string, image: File) {
+        const postData = new FormData();
+        postData.append("title", title);
+        postData.append("content", content);
+        postData.append("image", image, title);
+        this.http
+            .post<{ message: string, postId: string }>(
+                'http://localhost:3000/api/posts',
+                postData)
             .subscribe((createdPost) => {
-                console.log(createdPost.message);
-                post.id = createdPost.postId;
+                const post: Post = { id: createdPost.postId, title: title, content: content};
                 this.posts.push(post);
                 this.postsUpdated.next([...this.posts]);
                 this.router.navigate(["/"]);
