dcf9a9fbb59c67832c6a1823469e814c6b35bb92
Abhilash Kumar <abhilashkr31@gmail.com>
2018-12-25 17:15:11 +0530

    mean-stack: Add support for backend to handle signup requests

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-const postRoutes = require("./routes/posts")
+const postRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
 const app = express();
 mongoose.connect("mongodb+srv://abhilashkr:AlikVrkthhDdODNw@cluster0-otspd.mongodb.net/node-angular?retryWrites=true")
     .then(() => {
@@ -31,5 +33,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postRoutes);
+app.use("/api/user", postRoutes);
 
 module.exports = app;
