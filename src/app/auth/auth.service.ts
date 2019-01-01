a895b16cb620eff5638e2bce80899738a6e9ae2b
Abhilash Kumar <abhilashkr31@gmail.com>
2019-01-01 21:26:29 +0530

    mean-stack: Add logout button and improve UI after authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+
 import { Authdata } from "./auth-data.model";
 
 @Injectable({providedIn:'root'})
 export class AuthService {
     private token: string;
+    private authStatusListener = new Subject<boolean>();
 
     constructor(private http: HttpClient) {}
 
     getToken() {
         return this.token;
+    }
+
+    getAuthStatusListener() {
+        return this.authStatusListener.asObservable();
     }
 
     createUser(email: string, password: string) {
@@ -27,6 +34,7 @@ export class AuthService {
                 console.log(response);
                 const token = response.token;
                 this.token = token;
+                this.authStatusListener.next(true);
             })
     }
 }
