c77701e08bce913e70f661d51a7baedaf1762dd4
Abhilash Kumar <abhilashkr31@gmail.com>
2019-03-09 20:42:25 +0530

    mean-stack: Add timer to logout user after a certain period

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { Authdata } from "./auth-data.model";
 export class AuthService {
     private token: string;
     private isAuthenticated = false;
+    private tokenTimer: NodeJS.Timer;
     private authStatusListener = new Subject<boolean>();
 
     constructor(private http: HttpClient, private router: Router) {}
@@ -35,12 +36,16 @@ export class AuthService {
 
     loginUser(email: string, password: string) {
         const authData: Authdata = { email: email, password: password };
-        this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+        this.http.post<{token: string, expiresIn: number}>('http://localhost:3000/api/user/login', authData)
             .subscribe(response => {
                 console.log(response);
                 const token = response.token;
                 this.token = token;
                 if (token) {
+                    const expiresInDuration = response.expiresIn;
+                    this.tokenTimer = setTimeout(() => {
+                        this.logout();
+                    }, expiresInDuration * 1000);
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
                     this.router.navigate(['/']);
@@ -52,6 +57,7 @@ export class AuthService {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
+        clearTimeout(this.tokenTimer);
         this.router.navigate(['/']);
     }
 }
